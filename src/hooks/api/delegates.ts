3c71a4004506e91b579e935a3ac17a6a7f84c959
samdelaney <sam@thedelaneys.net>
2025-02-11 11:35:46 -0800

    fix delegate query bug

--- src/hooks/api/delegates.ts
+++ src/hooks/api/delegates.ts
@@ -1,13 +1,35 @@
 import { useQuery } from '@tanstack/react-query';
-import { queryDelegateGroups, ContractConfig } from 'hydra-auction-offchain';
+import { queryDelegateGroups, ContractConfig, DelegateGroupInfo } from 'hydra-auction-offchain';
 
 const DELEGATES_QUERY_KEY = 'delegates';
+
+const mockDelegateGroups: DelegateGroupInfo[] = [
+  {
+    delegateGroupId: 'mockDelegateGroupId',
+    delegateGroupMasterKeys: [],
+    delegateGroupServers: {
+      httpServers: ['http://localhost:3000'],
+      wsServers: ['ws://localhost:3000'],
+    },
+    delegateGroupMetadata: 'Ikigai Technologies',
+  },
+  {
+    delegateGroupId: 'mockDelegateGroupId2',
+    delegateGroupMasterKeys: [],
+    delegateGroupServers: {
+      httpServers: ['http://localhost:3001'],
+      wsServers: ['ws://localhost:3001'],
+    },
+    delegateGroupMetadata: 'MLabs',
+  }
+]
 
 export const useGetDelegates = (config: ContractConfig) => {
   const delegatesQuery = useQuery({
     queryKey: [DELEGATES_QUERY_KEY, config],
     queryFn: async () => {
-      return await queryDelegateGroups(config);
+      const groups = await queryDelegateGroups(config)
+      return groups ?? mockDelegateGroups;
     },
   });
   return delegatesQuery;
