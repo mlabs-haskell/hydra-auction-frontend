1d8758d64676ab53baa5484b05d3d39dce6f38e7
samdelaney <sam@thedelaneys.net>
2025-03-14 11:31:56 -0700

    fix walletVk signature spam

--- src/hooks/api/walletVk.ts
+++ src/hooks/api/walletVk.ts
@@ -1,18 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { ContractConfig, getWalletVk } from 'hydra-auction-offchain';
 import { getValidContractResponse } from 'src/utils/contract';
-import { BASE_REFETCH_INTERVAL } from 'src/utils/refetch';
+
+ // useQuery refetch interval was not working. Do it manually instead.
+let lastRequest = 0; // Track the last fetched time
+let refetchInterval = 1000 * 60 * 2; 
 
 const WALLET_VK_QUERY_KEY = 'wallet-vk';
 export const useWalletVk = (config: ContractConfig, enabled: boolean) => {
   return useQuery({
     queryKey: [WALLET_VK_QUERY_KEY, config],
     queryFn: async () => {
+      lastRequest = Date.now();
       const walletVkResponse = await getWalletVk(config);
       const walletVk = getValidContractResponse(walletVkResponse);
       return walletVk;
     },
-    staleTime: BASE_REFETCH_INTERVAL,
-    enabled: !!config && !!enabled,
+    retry: false,
+    enabled: !!config && !!enabled && Date.now() - lastRequest > refetchInterval, // Only refetch if enough time has passed
   });
 };
--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,7 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 10,
+      gcTime: 120,
+      refetchOnWindowFocus: false, // Disable refetch on window focus
+    },
+  },
+});
 
 export default function ReactQueryProvider({
   children,
